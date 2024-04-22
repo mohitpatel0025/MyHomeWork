ab08446974001597a69d203affe4f11728807851
Mohit Patel <mohit0025@yahoo.com>
2024-04-22 22:46:14 +0100

    my first commit

---
+++ ManualTopics.java
@@ -0,0 +1,48 @@
+package ListOfManualTopics;
+
+public class ManualTopics
+{
+    public static void main( String [] args)
+    {
+      System.out.println( " Manual Software Testing " );
+      System.out.println( " What is Software " );
+      System.out.println( " Software Risks " );
+      System.out.println( " What is Software Testing " );
+      System.out.println( " Benefits of Software Testing " );
+      System.out.println( " Who does Testing? " );
+      System.out.println( " Qualifications of a tester " );
+      System.out.println( " What is Manual Testing? " );
+      System.out.println( " SDLC " );
+      System.out.println( " STLC " );
+      System.out.println( " Challenges of Manual Testing ");
+      System.out.println( " Principles of Software Testing ");
+      System.out.println( " Test Plan and it’s benefits ");
+      System.out.println( " Test Plan Template ");
+      System.out.println( " Test Scenario + Template ");
+      System.out.println( " Test Case + Template ");
+      System.out.println( " Test Scenario vs Test Case ");
+      System.out.println( " Testing Techniques ");
+      System.out.println( " What is a bug? ");
+      System.out.println( " Bug Lifecycle ");
+      System.out.println( " Bug Template ");
+      System.out.println( " Severity Vs Priority ");
+      System.out.println( " Testing Levels ");
+      System.out.println( " White Box Testing ");
+      System.out.println(" Black Box Testing ");
+      System.out.println(" Static vs Dynamic Testing ");
+      System.out.println( " Exploratory Testing ");
+      System.out.println( " Ad-Hoc Testing ");
+      System.out.println( " Software Development Models ");
+      System.out.println( " Waterfall Model ");
+      System.out.println( " Iterative Model ");
+      System.out.println( " V- Model ");
+      System.out.println( " Verification and Validation ");
+      System.out.println( " Agile Model ");
+      System.out.println( " Definition of Ready/Done ");
+
+
+
+
+    }
+
+}
